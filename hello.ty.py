ecf9ef2a88f62767d9efcd03ef0af3346f327670
mpranav18 <pranav18x@gmail.com>
2025-03-18 14:55:51 +0530

    Add files via upload

---
+++ hello.ty.py
@@ -0,0 +1,186 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("pranav")
+pranav
+print(20)
+20
+
+===================================================================== RESTART: C:/Users/PRANAV/Desktop/hello.ty.py =====================================================================
+hello world!
+print(5+3)
+8
+print('hello world')
+hello world
+print('hello KGRCET')
+hello KGRCET
+print('welcome to python workshop')
+welcome to python workshop
+name=input("pranav:")
+pranav:
+print("hello,"+pranav)
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    print("hello,"+pranav)
+NameError: name 'pranav' is not defined
+print("hello,"+name)
+hello,
+print(name)
+
+name=input("please enter your name:)
+           
+SyntaxError: unterminated string literal (detected at line 1)
+name=input("please enter your name:")
+           
+please enter your name:pranav
+print(name)
+           
+pranav
+a=10
+           
+print(a)
+           
+10
+a=10
+           
+A=5
+           
+a
+           
+10
+A
+           
+5
+kgr=100
+           
+print(KGR)
+           
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    print(KGR)
+NameError: name 'KGR' is not defined
+kgr=KGR
+           
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    kgr=KGR
+NameError: name 'KGR' is not defined
+KGR=kgr
+           
+print(KGR)]
+SyntaxError: unmatched ']'
+>>> print(KGR)
+100
+>>> #KGR
+>>> A=1000#10
+>>> print
+<built-in function print>
+>>> print(A)
+1000
+>>> A="Bat"
+>>> ptint(A)
+Traceback (most recent call last):
+  File "<pyshell#31>", line 1, in <module>
+    ptint(A)
+NameError: name 'ptint' is not defined. Did you mean: 'print'?
+>>> name="pranav"
+>>> age=21
+>>> print("My name is"+ name +" and I am" +str(age) +" years old.")
+My name ispranav and I am21 years old.
+>>> print("My name is"+ name +" and I am " +str(age) +" years old.")
+My name ispranav and I am 21 years old.
+>>> print("My name is "+ name +" and I am " +str(age) +" years old.")
+My name is pranav and I am 21 years old.
+>>> print("My name is"+ name +" and I am" + (age) +" years old.")
+Traceback (most recent call last):
+  File "<pyshell#37>", line 1, in <module>
+    print("My name is"+ name +" and I am" + (age) +" years old.")
+TypeError: can only concatenate str (not "int") to str
+>>> city="HYDERABAD"
+>>> country="INDIA"
+>>> print("I live in " + city + ", " + country + " . "
+...       city="HYDERABAD"
+...       
+SyntaxError: '(' was never closed
+>>> 
+>>> SyntaxError: '(' was never closed
+...       
+SyntaxError: invalid syntax
+>>> print("I live in " + city + ", " + country + " . ")
+...       
+I live in HYDERABAD, INDIA . 
+>>> print("I live in " + city + ", " + country + " . ")
+...       
+I live in HYDERABAD, INDIA . 
+>>> 
+>>> 
+>>> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
